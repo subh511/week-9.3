55f7e16b1eb7b65e80ecf4b733f9ce96ecb0b43c
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-29 15:42:20 +0530

    .

--- src/generics1.ts
+++ src/generics1.ts
@@ -5,7 +5,7 @@ function firstElem<T>(arr:T[]){
 return arr[0]
 }
 
-const el = firstElem<string>(["Samrat", "Doe"]);
-const elem = firstElem<number>([1,2,3,4]);
+const el = firstElem(["Samrat", "Doe"]);
+const elem = firstElem([1,2,3,4]);
 console.log(elem)
 console.log(el.toLowerCase())
